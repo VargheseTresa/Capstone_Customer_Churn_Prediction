d1bdcce1bf7e0074e4ee4c7b13546d0d494868f3
Tresa Varghese <tresageorgev@gmail.com>
2021-12-22 12:15:58 -0800

    updates

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Contains the EDA performed on customer data to find behavior of customers who churn and Machine Learning Model to predict customer churn',
+    author='Tresa_Varghese',
+    license='MIT',
+)
